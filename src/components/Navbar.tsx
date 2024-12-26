62e5b6b1f48e9b7a05483a8778c2779080876502
arifulislam155 <arifulislam155@gmail.com>
2024-12-26 23:44:10 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="py-4 bg-white shadow-md text-gray-900 transition-colors duration-300">
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Navbar;
